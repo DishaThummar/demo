6458ee2fbdb23408cadb564aed3bfdd2b50045be
ishathummar <107744448+DishaThummar@users.noreply.github.com>
2024-03-11 16:03:46 +0530

    Revert "initial_commit"
    This reverts commit efcd6ab391e547e7f929dba39b07f7161df969b0.

--- android/app/src/main/kotlin/com/example/resumee/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.resumee
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/controller/home_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:get/get.dart';
-
-class HomeController extends GetxController {
-  RxInt selectedBottomIndex = 0.obs;
-
-}
